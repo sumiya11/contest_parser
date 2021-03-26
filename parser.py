5ca67b3f023160593a76c1295525d680a9f25c4d
Alexander Demin <60229118+sumiya11@users.noreply.github.com>
2021-03-26 09:06:19 +0300

    added KR mode and .userignore

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ import sys
 import os
 import json
 import argparse
+import re
 
 from dateutil import parser
 from datetime import datetime
@@ -24,6 +25,8 @@ def parseargs():
     parser.add_argument('-i','--input', help='Path to contest dump file', required=True)
     parser.add_argument('-d','--deadlines', help='Path to deadlines file', required=True)
     parser.add_argument('-o','--output', help='Output filename', required=True)
+    parser.add_argument('-f','--filter', help='Take participants from the filename', default=None)
+    parser.add_argument('-k','--KR', help='Kontrolnaya rabota rules', default=False, action="store_true")
     return vars(parser.parse_args())
 
 
@@ -32,6 +35,8 @@ def main():
     input_file     = args["input"]
     deadlines_file = args["deadlines"]
     output_file    = args["output"]
+    filter_file    = args["filter"]
+    KR             = args["KR"]
 
     # parse deadlines
     with open(deadlines_file) as inputs:
@@ -44,16 +49,35 @@ def main():
     # parse the dump
     with open(input_file, 'r') as inputs:
         soup = BeautifulSoup(inputs, 'xml')
-    
+
+
+    # parse the file with logins
+    if filter_file:
+        with open(filter_file, 'r') as inputs:
+            # valid login --> user group
+            sep = re.compile("[\t ]+")
+            valid_logins = {} 
+            for line in inputs.readlines():
+                line = re.split(sep, line)
+                login, group = line[0].strip(), line[-1].strip()
+                valid_logins[login] = group
+
     # problem titles
     problems = [
         problem["title"]
         for problem in soup.find_all("problem")
     ]
     
-    # iserid --> (name, problems score, total score)
-    userid_to_data = {
-        user["id"] : { 
+    # additional 
+    # userid --> userlogin
+    id_to_login = {
+        user["id"] : user["loginName"]
+        for user in soup.find_all("user")
+    }
+
+    # userlogin --> (name, problems score, total score)
+    login_to_data = {
+        user["loginName"] : { 
             "name" : user["displayedName"],
             "problems" : {
                 title : 0.
@@ -67,6 +91,7 @@ def main():
     
     for submit in soup.find_all("submit", verdict="OK"):
         userid = submit["userId"]
+        userlogin = id_to_login[userid]
         absolute_time = int(submit["absoluteTime"])
         problem_title = submit["problemTitle"]
         score = float(submit["score"])
@@ -78,26 +103,32 @@ def main():
             if absolute_time < deadline:
                 max_scale = max(max_scale, scaling)
         # we want the best result so far
-        userid_to_data[userid]["problems"][problem_title] = max(
+        login_to_data[userlogin]["problems"][problem_title] = max(
                 # and give 0.5 for non-positive OKs
-                max_scale * score if score > 0 else 0.5,
-                userid_to_data[userid]["problems"][problem_title],
+                max_scale * score if score > 0 else (0.5 if not KR else 0),
+                login_to_data[userlogin]["problems"][problem_title],
         )
 
     # to sum it up
-    for user in userid_to_data.values():
+    for user in login_to_data.values():
         user["total"] = sum(user["problems"].values())
 
     
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
+
     
-    # output file name is derived from the contest name 
-    # output_file = soup.find("contestLog").settings.contestName.contents[0]
-    # !!! From now on filename must be passed to args
+    # we ignore users who are not from the given list
+    if filter_file:
+        for login in list(login_to_data.keys()):
+            if login not in valid_logins:
+                login_to_data.pop(login)
+            else:
+                login_to_data[login]["group"] = valid_logins[login]
+
 
     with open(output_folder + "/" + output_file + output_ext, 'w') as outputs:
-        json.dump(userid_to_data, outputs, ensure_ascii=False, indent="\t")        
+        json.dump(login_to_data, outputs, ensure_ascii=False, indent="\t")        
 
     return 0
 
--- uploader.py
+++ uploader.py
@@ -7,22 +7,24 @@ import argparse
 
 
 """
-    Output positions are sorted by least lexicographical order 
-    on students names
+    Output positions are sorted by group. Within the group the order is
+    least lexicographical on students names
+
     The number of Students is not constant, so, we must look up
     the table with each update to determine the order
 """
 
 
 sheet_url   = "https://docs.google.com/spreadsheets/d/1ONirZzjx7I0OBfYtKVowg7xQMoRhieTRrIhY6a6nJgs/edit#gid=0"
-ru_alphabet = "\n АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+
 
 
 def initialize_headers(sh):
     headers = [[
-        "Фамилия, Имя, Отчество",
-        "ДЗ 1", "ДЗ 2", "ДЗ 3", "ДЗ 4", "ДЗ 5", "ДЗ 6", "ДЗ 7", "ДЗ 8", "Одз", " ",
-        "КР 1", "КР 2", "КР 3", "КР 4","Окр"
+        "Фамилия, Имя, Отчество", "Группа",
+        "ДЗ 1", "ДЗ 2", "ДЗ 3", "ДЗ 4", "ДЗ 5", "ДЗ 6", "ДЗ 7", "ДЗ 8", "Одз",
+        "КР 1", "КР 2", "КР 3", "КР 4", "Окр",
+        "", "Онакоп"
         ]
     ]
     formats = {
@@ -35,28 +37,43 @@ def initialize_headers(sh):
     sh.clear()
     sh.update(f"A1:{chr(ord('A')+len(headers[0]))}1", headers)
     sh.format(f"A1:{chr(ord('A')+len(headers[0]))}1", formats)
-    sh.freeze(1, 1)
-    # TODO: init formulas
+    sh.freeze(1, 2)
 
 
 def initialize_names(sh, data):
-    names = [[student["name"]] for student in data]
-    sh.update(f"A2:A{len(names)+1}", names)
+    names = [[student["name"], student["group"]] for student in data]
+    sh.update(f"A2:B{len(names)+1}", names)
 
 
-def update(sh, data, number):
+def add_points(sh, data, number):
     """
     params:
-        number -- the number of assignment to be updated
+        number -- the number of contest to be updated
+        where `1` stands for the first HW
+        and   `10` for the first KR   
     """
+
+    # take name ordering from the table
     names  = sh.col_values(1)[1:]
     totals = [[0] for _ in range(len(names))]
     for student in data:
+        if not student["name"] in names:
+            # new student added in the following contest
+            # we can explicitly stack him to the end of the table
+            names += [ student["name"] ]
+            totals += [ [0] ]
+            sh.update(
+                    f"A{len(names) + 1}:B{len(names) + 1}",
+                    [[student["name"], 0]]
+            )
+        # set the score
         idx = names.index(student["name"])
         totals[idx][0] = student["total"]
 
-    col = chr(ord('A') + number)
-    sh.update(f"{col}2:{col}{len(totals)+1}", totals)
+    # column index to be updated
+    shift = 1
+    col = chr(ord('A') + number + shift)
+    sh.update(f"{col}{2}:{col}{len(totals) + 1}", totals)
 
 
 def parse(filename):
@@ -64,16 +81,8 @@ def parse(filename):
         data = json.load(fp=inputs, encoding="utf-8")
 
     data = list(data.values())
-    data.sort(key=lambda student: student["name"])
+    data.sort(key=lambda student: (len(student["group"]), student["group"], student["name"]))
 
-    # ignore non-ru nicknames
-    # this is controversial
-    # would be better to have some kind of ignore list for logins
-    data = list(filter(
-        lambda student:
-            all(ch in ru_alphabet for ch in student["name"]),
-        data
-    ))
     return data
 
 
@@ -82,14 +91,16 @@ def parseargs():
     parser.add_argument('-i','--input', help='Path to parsed data file', required=True)
     parser.add_argument('-n','--number', help='The number to be updated', required=True)
     parser.add_argument('-c','--create', help='Create table from scratch', default=False, action="store_true")
+
     return vars(parser.parse_args())
+
 
 def main():
     args = parseargs() 
     filename = args["input"]
     number   = int(args["number"])
     create   = args["create"]
-    
+
     # -------
 
     gc = gspread.service_account(filename="credentials.json")
@@ -103,14 +114,14 @@ def main():
     # -------
 
     if create:
-        initialize_headers(sh)
+        if False:
+            initialize_headers(sh)
         initialize_names(sh, data)
     
-    # -------
+    # ------
 
     if True:
-        update(sh, data, number)
-
+        add_points(sh, data, number)
 
     return 0
 
